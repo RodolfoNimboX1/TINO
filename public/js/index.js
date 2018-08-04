f6daaa90032f06613054f3b5339aaaf57a718bbb
raquelmv <raquel.mvillarreal@gmail.com>
2018-08-04 13:04:33 -0500

    movement

--- public/js/index.js
+++ public/js/index.js
@@ -41,8 +41,8 @@ function getClients(){
       dataType: "json",
       cache: false,
       success: function(data, textStatus, xhr){
-        console.log("works");
-        console.log(data);
+        // console.log("works");
+        // console.log(data);
         resolve(data);
       },
       error: function(xhr, textStatus, errorThrown){
@@ -52,56 +52,82 @@ function getClients(){
   }).then(function(data, err){
     console.log(data);
     for(let i = 0; i< data.length; i ++){
-      console.log(data[i]);
+      // console.log(data[i]);
       var load = loadClients(data[i])  
-      console.log(load)
+      // console.log(load)
       $("#client-table").append(load);
     }
     return data;
   })
 };
 
-function loadMovements(movement){
+function loadMovements(Movements){
+  console.log(Movements.amount, "---------------PNMREP--------");
       var tableRow = $("<tr>");
-      var dataAmount = $("<td>").text(movement.amount);
-      var dataStatus = $("<td>").text(movement.status);
-      var dataConcept = $("<td>").text(movement.fullname);
-      var dataDate = $("<td>").text(movement.dateofpayment);
+      var dataAmount = $("<td>").text(Movements.amount);
+      var dataStatus = $("<td>").text(Movements.status);
+      var dataConcept = $("<td>").text(Movements.fullname);
+      var dataDate = $("<td>").text(Movements.dateofpayment);
       tableRow.append(dataAmount);
       tableRow.append(dataStatus);
       tableRow.append(dataConcept);
       tableRow.append(dataDate);
       
-
+ 
   return tableRow;
 }
 // get movements 
 function getMovements(){
-  return new Promise(function(resolve, reject){
-    $.ajax({
-      url: "/api/movements",
-      type: "get",
-      dataType: "json",
-      cache: false,
-      success: function(data, textStatus, xhr){
-        console.log("works");
-        console.log(data);
-        resolve(data);
-      },
-      error: function(xhr, textStatus, errorThrown){
-        reject("error");
-      }
-    })
-  }).then(function(data, err){
-    console.log(data);
-    for(let i = 0; i< data.length; i ++){
-      console.log(data[i]);
-      var load = loadMovements(data[i])  
-      console.log(load)
-      $("#client-table").append(loadMovements);
+  $.ajax({
+    url: "/api/movements",
+    type: "get",
+    dataType: "json",
+    cache: false,
+    success: function(data, textStatus, xhr){
+      // console.log("works" + "--------");
+      for(let i = 0; i< data.length; i ++){
+            // console.log(data[i], "//////////////////");
+            // console.log("works until here");
+            if (typeof(data[i] == "object")) {
+              $("#all-movements").append(loadMovements(data[i]));
+              
+            }
+            // console.log(load)
+            // console.log("works until here ALSO");
+          }
+          return data;
+    },
+    error: function(xhr, textStatus, errorThrown){
+      reject("error");
     }
-    return data;
-  })
+  });
+  // return new Promise(function(resolve, reject){
+  //   $.ajax({
+  //     url: "/api/movements",
+  //     type: "get",
+  //     dataType: "json",
+  //     cache: false,
+  //     success: function(data, textStatus, xhr){
+  //       console.log("works");
+  //       console.log(data);
+  //       resolve(data);
+  //     },
+  //     error: function(xhr, textStatus, errorThrown){
+  //       reject("error");
+  //     }
+  //   })
+  // }).then(function(data, err){
+  //   // console.log(data, "-------");
+  //   for(let i = 0; i< data.length; i ++){
+  //     console.log(data[i] + "//////////////////");
+  //     // console.log("works until here");
+  //     var load = loadMovements(data[i])  
+  //     // console.log(load)
+  //     // console.log("works until here ALSO");
+  //     $("#all-movements").append(loadMovements);
+  //   }
+  //   return data;
+  // })
 };
 
 getMovements();
